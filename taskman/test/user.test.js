d332dab1e04d3829e089f6fafe4102c0b6eb852e
Raph Gallardo <draphick@gmail.com>
2020-07-07 06:11:25 -0700

    More test cases section 144

--- taskman/test/user.test.js
+++ taskman/test/user.test.js
@@ -1,11 +1,18 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
+const userOneId = new mongoose.Types.ObjectId()
 const userOne = {
+    _id: userOneId,
     name: 'jerm',
     email: 'testjerm@example.com',
-    password: '56what!!'
+    password: '56what!!',
+    tokens: [{
+        token: jwt.sign({ _id: userOneId }, process.env.JWTSECRET)
+    }]
 }
 
 beforeEach(async () => {
@@ -13,9 +20,9 @@ beforeEach(async () => {
     await new User(userOne).save()
 })
 
-afterEach(() => {
-    console.log('after each')
-})
+// afterEach(() => {
+//     console.log('after each')
+// })
 
 test('Should signup a new user', async () => {
     await request(app).post('/users').send({
@@ -25,16 +32,48 @@ test('Should signup a new user', async () => {
     }).expect(201)
 })
 
-test('login success user', async () => {
+test('login success', async () => {
     await request(app).post('/users/login').send({
         email: userOne.email,
         password: userOne.password
     }).expect(200)
 })
 
-test('login failure user', async () => {
+test('login failure', async () => {
     await request(app).post('/users/login').send({
         email: userOne.email,
         password: 'asdf'
     }).expect(400)
+})
+
+test('get user profile success', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('authorization',`Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('get user profile failure', async () => {
+    await request(app)
+        .get('/users/me')
+        .set('authorization',`Bearer badauthtoken`)
+        .send()
+        .expect(401)
+})
+
+test('delete account success', async () => {
+    await request(app)
+        .delete('/users/me')
+        .set('authorization',`Bearer ${userOne.tokens[0].token}`)
+        .send()
+        .expect(200)
+})
+
+test('delete account failure', async () => {
+    await request(app)
+        .delete('/users/me')
+        .set('authorization',`Bearer badauthtoken`)
+        .send()
+        .expect(401)
 })
